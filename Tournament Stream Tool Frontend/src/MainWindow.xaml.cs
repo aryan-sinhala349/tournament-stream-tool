fadd81e23f5eadadd3f93f1bf24a824e7fd83e6a
Aryan Sinha <aryananusinha@gmail.com>
2024-01-25 14:11:25 -0500

    Added the ability to change Tournament and Team info in the frontend

--- Tournament Stream Tool Frontend/src/MainWindow.xaml.cs
+++ Tournament Stream Tool Frontend/src/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace TST
 {
@@ -7,6 +10,69 @@ namespace TST
         public MainWindow()
         {
             InitializeComponent();
+
+            /* Load Tournament Information */
+            TournamentNameTextBox.Text = ProgramState.GetTournamentName();
+            EventNameTextBox.Text = ProgramState.GetEventName();
+
+            /* Load Set Information*/
+            //Team 1
+            Team1CheckBox.IsChecked = ProgramState.IsTeamLosers(0);
+            Team1NameTextBox.Text = ProgramState.GetTeamName(0);
+
+            //Team 2
+            Team2CheckBox.IsChecked = ProgramState.IsTeamLosers(1);
+            Team2NameTextBox.Text = ProgramState.GetTeamName(1);
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProgramState.SaveProgramState();
+        }
+
+        private void MainGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainGrid.Focus();
+        }
+
+        private void TournamentNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProgramState.SetTournamentName(TournamentNameTextBox.Text);
+        }
+
+        private void EventNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProgramState.SetEventName(EventNameTextBox.Text);
+        }
+
+        private void Team1CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ProgramState.SetTeamLosers(0, true);
+        }
+
+        private void Team1CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ProgramState.SetTeamLosers(0, false);
+        }
+
+        private void Team1NameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProgramState.SetTeamName(0, Team1NameTextBox.Text);
+        }
+
+        private void Team2CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ProgramState.SetTeamLosers(1, true);
+        }
+
+        private void Team2CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ProgramState.SetTeamLosers(1, false);
+        }
+
+        private void Team2NameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProgramState.SetTeamName(1, Team2NameTextBox.Text);
         }
     }
 }
